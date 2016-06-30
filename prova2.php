1bc4a8c51e15583398bc1e4b08a7ecfaadf52502
GioeleRiva <jollygio93@gmail.com>
2016-06-30 13:48:54 +0200

    ciao

--- prova2.php
+++ prova2.php
@@ -1,27 +1,28 @@
 
 <?php
 
-	if ($_REQUEST["type"]=="prova"){
-		$conn = new mysqli('localhost', 'utente', 'password', 'STIM');
+	if ($_GET["type"]=="prova"){
+		$conn = new mysqli('localhost', 'utente', 'password', 'stim');
 		// Check connection
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
-            nn va bene
 		} 
      
     else {
-		$sql = "SELECT * FROM assistenza ORDER BY idAssistenza asc";
+		$sql = "SELECT idAssistenza FROM assistenza ORDER BY idAssistenza";
 		$result = $conn->query($sql);
-		$categories= array();
+        
+
 		if ($result->num_rows > 0) {
+            $categories= array();
 			// output data of each row
 			while($row = $result->fetch_assoc()) {
 				$categories[]=$row;
 			}
 		} else {
-			echo "0 results";
 		}
-		echo json_encode($categories);
+        echo json_encode($categories);
+        $result->close();
 		$conn->close();
 	}}
 // inclusione del file di classe
@@ -29,3 +30,4 @@
 
  
 // chiusura della connessione a MySQL
+?>
